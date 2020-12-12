2c1c1a09993e36c7a7ca0894d1dc7363321847ed
Irisawa Masaru <mass10.github@gmail.com>
2020-12-12 17:27:48 +0900

    Update application.rs

--- src/application.rs
+++ src/application.rs
@@ -1,26 +1,23 @@
 use super::configuration;
-use super::step_controller;
-/// ステップコントローラー
+use super::task_controller;
 
 /// アプリケーション本体の定義
 pub struct Application {}
 
 impl Application {
 	/// 指定された rmake ファイルを実行します。
-	pub fn start(&self, path: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
+	pub fn start(&self, path: &str, target_task_name: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
 		// コンフィギュレーション
 		let conf = configuration::Configuration::new(path);
 		if conf.is_err() {
 			println!("[ERROR] configuration error. {}", conf.err().unwrap());
 			return Ok(());
 		}
-
 		let conf = conf.ok().unwrap();
 
-		println!("[TRACE] {:?}", conf.settings);
-
-		let step_controller = step_controller::StepController::new(conf.steps);
-		step_controller.run("")?;
+		// タスクを実行
+		let step_controller = task_controller::TaskController::new(conf.steps);
+		step_controller.run(target_task_name)?;
 
 		return Ok(());
 	}
