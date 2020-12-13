4fd3f7c8aaef29e6cbdc1e269bfe29ed8d9d9267
Irisawa Masaru <mass10.github@gmail.com>
2020-12-13 09:27:33 +0900

    Update application.rs

--- src/application.rs
+++ src/application.rs
@@ -6,18 +6,18 @@ pub struct Application {}
 
 impl Application {
 	/// 指定された rmake ファイルを実行します。
-	pub fn start(&self, path: &str, target_task_name: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
+	pub fn start(&self, rmakefile_path: &str, target_task_name: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
 		// コンフィギュレーション
-		let conf = configuration::Configuration::new(path);
+		let conf = configuration::Configuration::new(rmakefile_path);
 		if conf.is_err() {
-			println!("[ERROR] configuration error. {}", conf.err().unwrap());
+			println!("[ERROR] {}", conf.err().unwrap());
 			return Ok(());
 		}
 		let conf = conf.ok().unwrap();
 
 		// タスクを実行
-		let step_controller = task_controller::TaskController::new(conf.steps);
-		step_controller.run(target_task_name)?;
+		let controller = task_controller::TaskController::new(conf.tasks);
+		controller.run(target_task_name)?;
 
 		return Ok(());
 	}
