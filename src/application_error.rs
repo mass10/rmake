f8ce0a726134e89253d151f2edc415157b196b16
Irisawa Masaru <mass10.github@gmail.com>
2020-12-26 09:16:31 +0900

    Create application_error.rs

---
+++ src/application_error.rs
@@ -0,0 +1,26 @@
+///
+/// Application defined error
+///
+#[derive(Debug, Clone)]
+pub struct ApplicationError {
+	pub description: String,
+}
+
+impl ApplicationError {
+	#[allow(unused)]
+	pub fn new(description: String) -> ApplicationError {
+		return ApplicationError { description: description };
+	}
+}
+
+impl std::fmt::Display for ApplicationError {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+		write!(f, "{}", self.description)
+	}
+}
+
+impl std::error::Error for ApplicationError {
+	fn description(&self) -> &str {
+		&self.description
+	}
+}
