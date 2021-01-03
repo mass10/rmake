b8f9a6f06bc51e6ac32539c3543e3b9939b11400
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-01-03 12:45:08 +0900

    Update status_holder.rs

--- src/status_holder.rs
+++ src/status_holder.rs
@@ -21,13 +21,11 @@ impl StatusHolder {
 	}
 
 	/// Set task status.
-	#[allow(unused)]
 	pub fn set_status(&mut self, name: &str, status: &str) {
 		self.status.insert(name.to_string(), status.to_string());
 	}
 
 	/// Return task status.
-	#[allow(unused)]
 	pub fn get_status(&mut self, name: &str) -> String {
 		let status = self.status.get(&name.to_string());
 		if status.is_none() {
