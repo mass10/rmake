982a6763b621c09dc3d2ff80205c2167c825cd31
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-01-16 10:19:32 +0900

    Update status_holder.rs

--- src/status_holder.rs
+++ src/status_holder.rs
@@ -15,17 +15,30 @@ impl StatusHolder {
 	}
 
 	/// Return mutable reference to the internal collection.
+	///
+	/// ### Returns
+	/// Mutable reference to the internal collection.
 	#[allow(unused)]
 	pub fn get_map(&mut self) -> &mut std::collections::HashMap<String, String> {
 		return &mut self.status;
 	}
 
 	/// Set task status.
+	///
+	/// ### Arguments
+	/// * `name` Task name
+	/// * `status` Task status
 	pub fn set_status(&mut self, name: &str, status: &str) {
 		self.status.insert(name.to_string(), status.to_string());
 	}
 
 	/// Return task status.
+	///
+	/// ### Arguments
+	/// * `name` Task name
+	///
+	/// ### Returns
+	/// The status of task named `name`
 	pub fn get_status(&mut self, name: &str) -> String {
 		let status = self.status.get(&name.to_string());
 		if status.is_none() {
