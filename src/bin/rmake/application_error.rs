40ca15f85404870f6d68e71975b43a903c7b7287
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-01-16 11:33:48 +0900

    Create application_error.rs

---
+++ src/bin/rmake/application_error.rs
@@ -0,0 +1,32 @@
+///
+/// Application defined error.
+///
+#[derive(Debug, Clone)]
+pub struct ApplicationError {
+	/// Description
+	pub description: String,
+}
+
+impl ApplicationError {
+	/// Create a new instance.
+	pub fn new(description: &str) -> ApplicationError {
+		return ApplicationError { description: description.to_string() };
+	}
+}
+
+impl std::fmt::Display for ApplicationError {
+	/// Write description to formatter.
+	///
+	/// ### Arguments
+	/// * `f` Target to write
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+		write!(f, "{}", self.description)
+	}
+}
+
+impl std::error::Error for ApplicationError {
+	/// Description.
+	fn description(&self) -> &str {
+		&self.description
+	}
+}
